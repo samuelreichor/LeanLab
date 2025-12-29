95521f5b37cf4134041c37d1538ba599a657d858
samuelreichor <samuelreichor@gmail.com>
2025-12-29 23:51:19 +0100

    Add gtm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,6 +26,15 @@ export default defineNuxtConfig({
         { rel: 'shortcut icon', href: '/favicons/favicon.ico' },
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-touch-icon.png' },
         { rel: 'manifest', href: '/favicons/site.webmanifest' }
+      ],
+      script: [
+        {
+          innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NTZ8T3WH');`
+        }
       ]
     }
   },
