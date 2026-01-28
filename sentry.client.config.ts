7605af6ab32ca065c27d51a032c1eb28e44b2169
samuelreichor <samuelreichor@gmail.com>
2026-01-28 19:49:03 +0100

    fix: sentry init gdpr issues

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,32 +1,43 @@
 import * as Sentry from '@sentry/nuxt'
 
-Sentry.init({
-  // If set up, you can use your runtime config here
-  // dsn: useRuntimeConfig().public.sentry.dsn,
+declare global {
+  interface Window {
+    Cookiebot?: {
+      consent: {
+        necessary: boolean
+        preferences: boolean
+        statistics: boolean
+        marketing: boolean
+      }
+    }
+  }
+}
+
+const sentryConfig = {
   dsn: 'https://ce1d6c42ccf4f1b934dc4937722fd573@o4510657224769536.ingest.de.sentry.io/4510657225949264',
-
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
   tracesSampleRate: 1.0,
-
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
   replaysSessionSampleRate: 0.1,
-
-  // If the entire session is not sampled, use the below sample rate to sample
-  // sessions when an error occurs.
   replaysOnErrorSampleRate: 1.0,
-
-  // If you don't want to use Session Replay, just remove the line below:
   integrations: [Sentry.replayIntegration()],
-
-  // Enable logs to be sent to Sentry
   enableLogs: true,
-
-  // Enable sending of user PII (Personally Identifiable Information)
-  // https://docs.sentry.io/platforms/javascript/guides/nuxt/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
-
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false
-})
+}
+
+// Check if Cookiebot consent for statistics is already given
+const hasConsent = typeof window !== 'undefined'
+  && window.Cookiebot?.consent?.statistics
+
+// Initialize Sentry only if consent is already given
+if (hasConsent) {
+  Sentry.init(sentryConfig)
+}
+
+// Listen for consent changes
+if (typeof window !== 'undefined') {
+  window.addEventListener('CookiebotOnAccept', () => {
+    if (window.Cookiebot?.consent?.statistics && !Sentry.getClient()) {
+      Sentry.init(sentryConfig)
+    }
+  })
+}
